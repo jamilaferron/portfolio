2a9c92e6905ba21a08e5d8d892d2b5c763f6d0c1
jamilaferron <jamila_ferron@hotmail.co.uk>
2019-03-18 16:59:24 +0000

    made changes to phpScript/viewAboutMe.php and .gitignore completed unit testing

--- phpScripts/viewAboutMe.php
+++ phpScripts/viewAboutMe.php
@@ -1,18 +1,26 @@
 <?php
 
 
+	/**
+	 * @param PDO $db
+	 * @return array
+	 */
 	function getAboutMe (PDO $db) : array {
 		$query = $db->prepare('SELECT `paragraph` FROM `paragraphs` WHERE `deleted` = 0;');
 		$query->execute();
 		return $query->fetchAll();
 	}
 
+	/**
+	 * @param array $paragraphs
+	 * @return string
+	 */
 	function viewParagraphs (array $paragraphs) : string {
-		$result = '';
+		$string = '';
 		foreach ($paragraphs as $paragraph) {
 			if (!in_array('', $paragraph)) {
-				$result .= '<p>' . $paragraph['paragraph'] . '</p>';
+				$string .= '<p>' . $paragraph['paragraph'] . '</p>';
 			}
 		}
-		return $result;
+		return $string;
 	}
