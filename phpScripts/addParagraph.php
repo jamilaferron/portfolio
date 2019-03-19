8efa0bc3db9b3654d341e10c994d3953f673f319
jamilaferron <jamila_ferron@hotmail.co.uk>
2019-03-19 13:21:58 +0000

    added a script to add patagraphs to database

---
+++ phpScripts/addParagraph.php
@@ -0,0 +1,21 @@
+<?php
+
+
+	/**
+	 * This function takes a string submitted from a form and inserts it into the database
+	 *
+	 * @param PDO $db accepts a db connection variable as an input
+	 * @param string $paragraph accepts a string submitted via a form as an input
+	 * @return String confirmation message
+	 */
+	function addParagraph (PDO $db, string $paragraph) : String {
+		if ($paragraph === '') {
+			return 'Please enter a paragraph';
+		} else {
+			$query = $db->prepare("INSERT INTO `paragraphs` (`paragraph`) VALUES (:paragraph);");
+			$query->execute(['paragraph'=>$paragraph]);
+			return 'Your Paragraph has been saved';
+		}
+	}
+
+
